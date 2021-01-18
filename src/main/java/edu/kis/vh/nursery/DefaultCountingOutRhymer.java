fda3c9377a81236519c3bd61509b3fa8b910c0e6
Piotr <eneme165@gmail.com>
2021-01-18 23:08:55 +0100

    poprawa stalych

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,11 +2,12 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    public static final int size = 12;
+    public static final int SIZE = 12;
     public static final int initialValue = -1;
-    private final int[] numbers = new int[size];
+    public static final int INITIAL_VALUE = initialValue;
+    private final int[] numbers = new int[SIZE];
 
-    public int total = initialValue;
+    public int total = INITIAL_VALUE;
 
     public void countIn(int in) {
         if (!isFull())
